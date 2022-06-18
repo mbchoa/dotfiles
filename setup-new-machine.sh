7143f03ac4d89a9d5b94d3628e0abc7edfea412c
Michael-Bryant Choa <mbchoa@gmail.com>
2022-06-17 19:13:40 -0700

    Remove Git section in setup-new-machine script

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -49,16 +49,6 @@ fi
 
 $HOME/dotfiles/install/node.sh
 
-
-###############################################################################
-# Git                                                                         #
-###############################################################################
-
-# github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-
 ###############################################################################
 # Symlinks to link dotfiles into ~/                                           #
 ###############################################################################
