5fb7a6fcb03116c8d4ef399d4541abc2ba93f3c2
Michael-Bryant Choa <mbchoa@gmail.com>
2022-06-17 19:49:57 -0700

    Add back brew install script to install pyenv

---
+++ install/brew.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Installs Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Install the Homebrew packages I use on a day-to-day basis.
+apps=(
+    pyenv
+)
+
+echo "Installing Brew apps"
+for i in "${apps[@]}"; do
+  echo "Installing $i"
+  brew install "$i"
+done
+
+# Remove outdated versions from the cellar
+brew cleanup
